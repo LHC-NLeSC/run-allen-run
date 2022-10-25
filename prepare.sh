6e0bcc4e2de7d932f69d95f8784faac95e21bdf2
Suvayu Ali <fatkasuvayu+linux@gmail.com>
2022-10-25 16:43:57 +0200

    script to generate JSON config

---
+++ prepare.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+declare builddir="$1"
+pushd "$builddir"
+
+declare branch=$(git branch --show-current) sequence
+
+[[ -n $branch ]] || \
+    {
+	echo "aborting, working directory in detached head"
+	exit
+    }
+
+if [[ branch == master ]]; then
+    sequence=hlt1_pp_default
+else
+    sequence=ghostbuster_test
+fi
+
+./toolchain/wrapper ./Allen -t 12 --events-per-slice 1000 -n 1000 -r 100 \
+		    --write-configuration 1 --sequence $sequence \
+		    --mdf /data/bfys/raaij/upgrade/MiniBrunel_2018_MinBias_FTv4_DIGI_retinacluster_v1.mdf
+popd
