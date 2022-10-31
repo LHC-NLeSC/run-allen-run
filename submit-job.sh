9e4b3bca5c8cdc2e00beb065af8243c173f7ba9b
Suvayu Ali <fatkasuvayu+linux@gmail.com>
2022-10-31 22:31:39 +0100

    run master branch as baseline

--- submit-job.sh
+++ submit-job.sh
@@ -14,6 +14,19 @@ source venv/bin/activate
 mlflow experiments list 2>/dev/null | grep -q v100 || \
     mlflow experiments create -n v100
 
-python ./scanprops.py ../Allen/build/Sequence.json \
-       --experiment-name v100 \
-       --batch-size-range 256 16000
+if [[ -d ../Allen-master ]]; then
+    python ./scanprops.py ../Allen-master/build/Sequence.json \
+	   --experiment-name v100
+else
+    echo "Did you run ./build.sh?"
+    echo "No build of master branch found" > /dev/stderr
+fi
+
+if [[ -d ../Allen-ghostbuster ]]; then
+    python ./scanprops.py ../Allen-ghostbuster/build/Sequence.json \
+	   --experiment-name v100 \
+	   --batch-size-range 256 16000
+else
+    echo "Did you run ./build.sh?"
+    echo "No build of ghostbuster branch found" > /dev/stderr
+fi
