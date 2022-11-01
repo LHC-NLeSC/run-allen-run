c090f4418f98cf50fe4e8c70bca473031ba611bf
Suvayu Ali <fatkasuvayu+linux@gmail.com>
2022-11-01 06:50:13 +0100

    update build dir path in job submission script to match build.sh

--- submit-job.sh
+++ submit-job.sh
@@ -14,16 +14,16 @@ source venv/bin/activate
 mlflow experiments list 2>/dev/null | grep -q v100 || \
     mlflow experiments create -n v100
 
-if [[ -d ../Allen-master ]]; then
-    python ./scanprops.py ../Allen-master/build/Sequence.json \
+if [[ -d Allen-master ]]; then
+    python ./scanprops.py Allen-master/build/Sequence.json \
 	   --experiment-name v100
 else
     echo "Did you run ./build.sh?"
     echo "No build of master branch found" > /dev/stderr
 fi
 
-if [[ -d ../Allen-ghostbuster ]]; then
-    python ./scanprops.py ../Allen-ghostbuster/build/Sequence.json \
+if [[ -d Allen-ghostbuster ]]; then
+    python ./scanprops.py Allen-ghostbuster/build/Sequence.json \
 	   --experiment-name v100 \
 	   --batch-size-range 256 16000
 else
