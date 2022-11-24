7604c762301380e8a4ab788033bb79e1a6a0754b
Suvayu Ali <fatkasuvayu+linux@gmail.com>
2022-11-24 12:48:59 +0100

    submit-job.sh: live log current job

--- submit-job.sh
+++ submit-job.sh
@@ -6,6 +6,8 @@
 #PBS -l walltime=24:00:00
 #PBS -l mem=4gb
 
+echo "Job: ${PBS_JOBID}" |& tee current.log
+
 pushd /project/bfys/$USER/codebaby/run-allen-run || exit
 
 source lhcb-setup.sh
@@ -15,18 +17,18 @@ mlflow experiments list 2>/dev/null | grep -q v100 || \
     mlflow experiments create -n v100
 
 if [[ -d Allen-master ]]; then
-    echo "Job (master): hlt1_pp_default_seq.json"
+    echo "Job (master): hlt1_pp_default_seq.json" |& tee -a current.log
     python ./scanprops.py Allen-master/build/hlt1_pp_default_seq.json \
 	   --experiment-name v100
 else
-    echo "Did you run ./build.sh & ./prepare.sh?"
-    echo "No build of master branch found" > /dev/stderr
+    echo "Did you run ./build.sh & ./prepare.sh?" |& tee -a current.log
+    echo "No build of master branch found" |& tee -a current.log > /dev/stderr
 fi
 
 if [[ -d Allen-ghostbuster ]]; then
     for i in {1..5}; do
 	for f in nn nn_big; do
-	    echo "Job (ghostbuster): ${i} copies of ghostbuster_test_n${i}_seq.json"
+	    echo "Job (ghostbuster): ${i} copies of ghostbuster_test_n${i}_seq.json" |& tee -a current.log
 	    python ./scanprops.py Allen-ghostbuster/build/ghostbuster_test_n${i}_seq.json \
 		   --experiment-name v100 \
 		   --batch-size-range 256 16000 \
@@ -35,10 +37,10 @@ if [[ -d Allen-ghostbuster ]]; then
 		   --copies $i
 	done
     done
-    echo "Job (ghostbuster): hlt1_pp_default_seq.json"
+    echo "Job (ghostbuster): hlt1_pp_default_seq.json" |& tee -a current.log
     python ./scanprops.py Allen-ghostbuster/build/hlt1_pp_default_seq.json \
 	   --experiment-name v100
 else
-    echo "Did you run ./build.sh & ./prepare.sh?"
-    echo "No build of ghostbuster branch found" > /dev/stderr
+    echo "Did you run ./build.sh & ./prepare.sh?" |& tee -a current.log
+    echo "No build of ghostbuster branch found" |& tee -a current.log > /dev/stderr
 fi
