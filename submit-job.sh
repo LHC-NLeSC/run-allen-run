7cbe50a78777d9961eaebc033b4de87d5b6dfabd
Suvayu Ali <fatkasuvayu+linux@gmail.com>
2023-01-20 14:13:41 +0100

    submit-job.sh: refactor for better error handling

--- submit-job.sh
+++ submit-job.sh
@@ -16,25 +16,45 @@ source venv/bin/activate
 mlflow experiments search 2>/dev/null | grep -q v100 || \
     mlflow experiments create -n v100
 
-if [[ -d Allen-ghostbuster ]]; then
-    echo "Jobs w/ ghostbuster:" |& tee -a current.log
-    cp -v ./allen_benchmarks.py Allen-ghostbuster/configuration/python/AllenCore/ |& tee -a current.log
-    for i in {1..5}; do
-	for f in nn_tiny nn nn_big nn_bigger; do
-	    echo "Job (ghostbuster): ghostbuster_test_n${i}_seq.json" |& tee -a current.log
-	    python ./scanprops.py Allen-ghostbuster/build/ghostbuster_test_n${i}_seq.json \
-		   --experiment-name v100 \
-		   --batch-size-range 256 16000 \
-		   --fp16 \
-		   --no-infer \
-		   --onnx-input /project/bfys/$USER/codebaby/Allen/input/ghost_${f}.onnx \
-		   --copies $i |& tee -a current.log
-	done
+[[ -d Allen-ghostbuster/build ]] || \
+    {
+	echo "No build of ghostbuster branch found" |& tee -a current.log > /dev/stderr
+	echo "Did you run ./build.sh & ./prepare.sh?" |& tee -a current.log
+	exit 1
+    }
+
+echo "Jobs w/ ghostbuster:" |& tee -a current.log
+cp -v ./allen_benchmarks.py Allen-ghostbuster/configuration/python/AllenCore/ |& tee -a current.log
+
+for i in {1..5}; do
+    json_config=Allen-ghostbuster/build/ghostbuster_test_n${i}_seq.json
+    [[ -f ${json_config} ]] || \
+	{
+	    echo "${json_config}: missing" |& tee -a current.log > /dev/stderr
+	    echo "Did you run ./prepare.sh?" |& tee -a current.log
+	    exit 2
+	}
+
+    for f in nn_tiny nn nn_big nn_bigger; do
+	onnx_input=/project/bfys/$USER/codebaby/Allen/input/ghost_${f}.onnx
+	[[ -f ${onnx_input} ]] || \
+	    {
+		echo "${onnx_input}: missing" |& tee -a current.log > /dev/stderr
+		exit 3
+	    }
+
+	echo "config: ${json_config}" |& tee -a current.log
+	echo "onnx: ${onnx_input}" |& tee -a current.log
+	python ./scanprops.py ${json_config} \
+	       --experiment-name v100 \
+	       --batch-size-range 256 16000 \
+	       --fp16 \
+	       --no-infer \
+	       --onnx-input ${onnx_input} \
+	       --copies $i |& tee -a current.log
     done
-    echo "Job (ghostbuster): hlt1_pp_default_seq.json" |& tee -a current.log
-    python ./scanprops.py Allen-ghostbuster/build/hlt1_pp_default_seq.json \
+done
+
+echo "config: hlt1_pp_default_seq.json" |& tee -a current.log
+python ./scanprops.py Allen-ghostbuster/build/hlt1_pp_default_seq.json \
 	   --experiment-name v100 |& tee -a current.log
-else
-    echo "Did you run ./build.sh & ./prepare.sh?" |& tee -a current.log
-    echo "No build of ghostbuster branch found" |& tee -a current.log > /dev/stderr
-fi
